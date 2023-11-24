762f26bb474fb2afb87fe6f131920f1ddd03f846
Hiarina <113544780+1Hiarina@users.noreply.github.com>
2023-11-24 12:26:19 +0100

    Update fonction

--- functions.py
+++ functions.py
@@ -44,6 +44,22 @@ def remove_punctuation():
 
             file.write(txt)
 
+
+def list_of_word():
+
+    list = list_of_files("./cleaned", ".txt")
+
+    list_of_word = []
+
+    for file in list:
+        Tf = TF(file)
+        for word in Tf.keys():
+            if word not in list_of_word:
+                list_of_word.append(word)
+
+    return list_of_word
+
+
 # Fonctions TF-IDF
 def TF(file):
     with open(f"./cleaned/{file}", 'r') as f:
@@ -81,77 +97,30 @@ def TF_IDF(word, file):
 
     tf = TF(file)
     idf = IDF("./cleaned")
-    TF_IDF = tf[word] * idf[word]
+    if word in tf and word in idf:
+        TF_IDF = tf[word] * idf[word]
+    else:
+        TF_IDF = 0.0
 
     return TF_IDF
 
-def Str_file(file):
-    with open(f"./cleaned/{file}", 'r') as f:
-        contenu = f.read()
-        return contenu
-def Matrix_TF_IDF(directory):
+def Matrice(Directory):
 
+    list_word = list_of_word()
+    list_files = list_of_files("./cleaned",".txt")
 
-    Matrix_tf_idf = []
+    Matrix = []
 
-    file_list = list_of_files(directory, "txt")
-
-    for i in range(len(file_list)):
-        if i == 4:
-            pass
-        Matrix_tf_idf.append([])
-        file = file_list[i]
-        dic_file = TF(file)
-        for word in dic_file:
+    for i, word in enumerate(list_word):
+        row = [word]
+        for file in list_files:
             tf_idf = TF_IDF(word, file)
-            Matrix_tf_idf[i].append(word + " : " + str(tf_idf))
-        """
-        StrDoc = Str_file(file).split()
-        print(StrDoc)
-        print(len(Matrix_tf_idf[i]), len(StrDoc))
-        """
-    return Matrix_tf_idf
+            row.append(tf_idf)
+        Matrix.append(row)
+
+    return Matrix
 
 
-def Transpose_Matrix(matrice):
-    #Initialisation du nombre de ligne et de colonnes
-    nombre_lignes = len(matrice)
-    nombre_colonnes = max(len(ligne) for ligne in matrice)
 
-    #Initialiser de la matrice transposée
-    matrice_transposee = [[] for i in range(nombre_colonnes)]
 
-    #Remplissage de la matrice
-    for i in range(nombre_lignes):
-        for j in range(len(matrice[i])):
-            matrice_transposee[j].append(matrice[i][j] if j < len(matrice[i]) else None)
-    return matrice_transposee
 
-def transfo_dic_list(dic):
-    T = []
-    for key in dic.keys():
-        T.append(key)
-    return T
-def Mot_via_scoreTFIDF(directory,i,j):
-    list = list_of_files(directory, "txt")
-    file = list[j]
-    tf = TF(file)
-    tf = transfo_dic_list(tf)
-    print(tf, "\n", len(tf), i, file)
-    mot = tf[i]
-    return mot
-#print(Mot_via_scoreTFIDF("./cleaned",3,4))
-
-Matrix = Matrix_TF_IDF("./cleaned")
-for i in range(len(Matrix)):
-    print(Matrix[i])
-
-"""
-Matrix = Matrix_TF_IDF("./cleaned")
-Matrix_Transposee = Transpose_Matrix(Matrix)
-
-print("\n")
-
-for ligne in Matrix_Transposee:
-    print(ligne)
-    """
