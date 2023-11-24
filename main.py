59ba9c221c424cf3a062fa3b33a88c45706d74fa
Hiarina <113544780+1Hiarina@users.noreply.github.com>
2023-11-24 12:27:14 +0100

    Update fonction

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
 from functions import *
 
-Directory = "./cleaned"
-Matrix_tf_idf = Matrix_TF_IDF(Directory)
-list = list_of_files(Directory,".txt")
-
-L = []
-
-for i in range(len(Matrix_tf_idf)):
-    for j in range(len(Matrix_tf_idf[i])):
-        if Matrix_tf_idf[i][j] == 0.0:
-            dic_word = TF(list[i])
-            k = 1
-            for cle in dic_word.keys():
-                if k == j:
-                    L.append(cle)
-                else:
-                    k += 1
-
-
-
