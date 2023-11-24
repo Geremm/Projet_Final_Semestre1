b464981ac1c08bb4d804de0983831be73dc588d2
Geremm <146996279+Geremm@users.noreply.github.com>
2023-11-24 16:05:22 +0100

    Commit General

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 from functions import *
 
-#Matrix = Matrice_TF_IDF("./cleaned")
+Matrix_tf_idf = Matrice_TF_IDF("./cleaned")
+list_file = list_of_files("./cleaned",".txt")
+
+#fonctionalité 1
+list_of_non_important_words = []
+
+for i in range(len(Matrix_tf_idf)):
+    summ = 0
+    for j in range(1, len(Matrix_tf_idf[i])):
+        summ += Matrix_tf_idf[i][j]
+    if summ == 0:
+        list_of_non_important_words.append(Matrix_tf_idf[i][0])
+
+
+print(list_of_non_important_words)
+
+#fonctionalité 2
+Max_TF_idf = []
+
+Max = 0
+
+for i in range(len(Matrix_tf_idf)):
+    for j in range(1, len(Matrix_tf_idf[i])):
+        if Matrix_tf_idf[i][j] > Max:
+            Max = Matrix_tf_idf[i][j]
+
+list_of_biggest_TFIDF = []
+
+for i in range(len(Matrix_tf_idf)):
+    for j in range(1, len(Matrix_tf_idf[i])):
+        if Matrix_tf_idf[i][j] == Max:
+            list_of_biggest_TFIDF.append(Matrix_tf_idf[i][0])
+
+
+print(list_of_biggest_TFIDF)
+
+
+
+#Fonctionalité 3
 file1 = "Nomination_Chirac1.txt"
 file2 = "Nomination_CHirac2.txt"
 DicTxt1 = TF(file1)
@@ -18,6 +56,36 @@ for cle, val in DicChirac.items():
 print(T)
 print(max)
 
+
+
+#Fonctionalité 4
+
+
+Max = 0
+list_president_nation = []
+list_nation = []
+
+for file in list_file:
+    dic_word = TF(file)
+    for word in dic_word.keys():
+        if word == 'nation':
+            list_president_nation.append(file)
+            if dic_word['nation'] >= Max:
+                Max = dic_word['nation']
+
+for file in list_president_nation:
+    president = extractnames(file)
+    print(f"{president} à prononcé le mot Nation")
+
+for file in list_president_nation:
+    dic_word = TF(file)
+    if dic_word['nation'] == Max:
+        name = extractnames(file)
+        list_nation.append(name)
+
+print(list_nation)
+
+#Fonctionalité 5
 mot = "climat"
 Trouve = False
 i = 0
